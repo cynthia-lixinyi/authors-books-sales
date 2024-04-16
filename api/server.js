2497a001de37ee71a5d57e51d25794c6299acd4b
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-15 17:57:53 -0700

    Implemented the optional query parameter author_name

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const server = express();
 
 server.use(cors());
 server.use(helmet());
-server.use(express.json()); // parse incoming requests into JSON payloads 
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send('Welcome to the authors-books-sales app server!');
@@ -16,6 +16,30 @@ server.get('/', (req, res) => {
 // Get all top 10 performing authors
 server.get('/top-10-authors', async (req, res) => {
   try {
+    // Check if author_name query parameter is provided
+    // Ex. localhost:8888/top-10-authors?author_name=Sophia%20Thomas
+    const authorName = req.query.author_name;
+
+    // If author_name is provided, filter the top authors by author_name
+    if (authorName) {
+      const topAuthor = await db('authors')
+        .leftJoin('books', 'authors.id', 'books.author_id')
+        .leftJoin('sale_items', 'books.id', 'sale_items.book_id')
+        .select('authors.id', 'authors.author_name')
+        .sum({ sales_revenue: db.raw('sale_items.quantity * sale_items.item_price') })
+        .groupBy('authors.id')
+        .orderByRaw('SUM(sale_items.quantity * sale_items.item_price) DESC')
+        .where('authors.author_name', authorName)
+        .first();
+
+      if (!topAuthor) {
+        return res.status(404).json({ message: '404: The given author is not found in top 10 performing authors' });
+      } else {
+        return res.json(topAuthor);
+      }
+    }
+
+    // If author_name is not provided, return all top 10 authors
     const topAuthors = await db('authors')
       .leftJoin('books', 'authors.id', 'books.author_id')
       .leftJoin('sale_items', 'books.id', 'sale_items.book_id')
@@ -27,7 +51,8 @@ server.get('/top-10-authors', async (req, res) => {
     res.json(topAuthors);
   } catch (err) {
     console.log(err);
+    res.status(500).json({ message: '500: Internal server error' });
   }
-})
+});
 
 module.exports = server;
