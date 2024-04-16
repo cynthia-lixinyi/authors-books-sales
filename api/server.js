046c2c598e27681f271b8353f7f7defaad9ee714
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-15 18:17:32 -0700

    Update README.md

--- api/server.js
+++ api/server.js
@@ -13,42 +13,33 @@ server.get('/', (req, res) => {
   res.send('Welcome to the authors-books-sales app server!');
 })
 
+async function getTopTenAuthors(authorName) {
+  let query = db('authors')
+    .leftJoin('books', 'authors.id', 'books.author_id')
+    .leftJoin('sale_items', 'books.id', 'sale_items.book_id')
+    .select('authors.id', 'authors.author_name')
+    .sum({ sales_revenue: db.raw('sale_items.quantity * sale_items.item_price') })
+    .groupBy('authors.id')
+    .orderByRaw('SUM(sale_items.quantity * sale_items.item_price) DESC')
+    .limit(10);
+  if (authorName) {
+    // query = query.where('authors.author_name', authorName).first();
+    query = query.where(db.raw('lower(authors.author_name)'), 'like', '%' + authorName.toLowerCase() + '%').first();
+  }
+  return await query;
+}
+
 // Get all top 10 performing authors
 server.get('/top-10-authors', async (req, res) => {
   try {
-    // Check if author_name query parameter is provided
-    // Ex. localhost:8888/top-10-authors?author_name=Sophia%20Thomas
     const authorName = req.query.author_name;
-
-    // If author_name is provided, filter the top authors by author_name
-    if (authorName) {
-      const topAuthor = await db('authors')
-        .leftJoin('books', 'authors.id', 'books.author_id')
-        .leftJoin('sale_items', 'books.id', 'sale_items.book_id')
-        .select('authors.id', 'authors.author_name')
-        .sum({ sales_revenue: db.raw('sale_items.quantity * sale_items.item_price') })
-        .groupBy('authors.id')
-        .orderByRaw('SUM(sale_items.quantity * sale_items.item_price) DESC')
-        .where('authors.author_name', authorName)
-        .first();
-
-      if (!topAuthor) {
-        return res.status(404).json({ message: '404: The given author is not found in top 10 performing authors' });
-      } else {
-        return res.json(topAuthor);
-      }
+    const topTenAuthors = await getTopTenAuthors(authorName);
+    if (authorName && !topTenAuthors) {
+      return res.status(404).json({ 
+        message: '404: The given author in url is not found in top 10 performing authors' 
+      });
     }
-
-    // If author_name is not provided, return all top 10 authors
-    const topAuthors = await db('authors')
-      .leftJoin('books', 'authors.id', 'books.author_id')
-      .leftJoin('sale_items', 'books.id', 'sale_items.book_id')
-      .select('authors.id', 'authors.author_name')
-      .sum({ sales_revenue: db.raw('sale_items.quantity * sale_items.item_price') })
-      .groupBy('authors.id')
-      .orderByRaw('SUM(sale_items.quantity * sale_items.item_price) DESC')
-      .limit(10);
-    res.json(topAuthors);
+    res.json(topTenAuthors);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: '500: Internal server error' });
